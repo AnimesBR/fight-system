7df7225114bc2f3d6b41cfc8f28e53908898bbfe
André Teixeira <andtxr@gmail.com>
2017-02-04 14:34:54 -0200

    New seeds and migration.

---
+++ config/Seeds/CharactersSeed.php
@@ -0,0 +1,49 @@
+<?php
+use Migrations\AbstractSeed;
+use Cake\ORM\TableRegistry;
+
+/**
+ * Characters seed.
+ */
+class CharactersSeed extends AbstractSeed
+{
+
+    private function getUsers() {
+        $users = TableRegistry::get('Users');
+        return $users->find('seed');
+    }
+
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeds is available here:
+     * http://docs.phinx.org/en/latest/seeding.html
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $table = TableRegistry::get('characters');
+        $table->deleteAll([]);
+
+        $users = $this->getUsers()->toArray();
+        $faker = Faker\Factory::create();
+        $data = [];
+
+        for ($i=1; $i<=10; $i++) {
+            $data[] = [
+                'id' => $faker->uuid,
+                'user_id' => $users[array_rand($users)]['id'],
+                'name' => $faker->name,
+                'description' => $faker->realText(200),
+                'created' => $faker->date('Y-m-d H:i:s'),
+                'modified' => $faker->date('Y-m-d H:i:s')
+            ];
+        }
+
+        $table = $this->table('characters');
+        $table->insert($data)->save();
+    }
+}
---
+++ config/Seeds/UsersSeed.php
@@ -0,0 +1,45 @@
+<?php
+use Migrations\AbstractSeed;
+
+use Cake\Auth\DefaultPasswordHasher;
+use Cake\ORM\TableRegistry;
+
+/**
+ * Users seed.
+ */
+class UsersSeed extends AbstractSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeds is available here:
+     * http://docs.phinx.org/en/latest/seeding.html
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $table = TableRegistry::get('Users');
+        $table->deleteAll([]);
+
+        $faker = Faker\Factory::create();
+        $data = [];
+
+        for ($i=1; $i<=10; $i++) {
+            $data[] = [
+                'id' => $faker->uuid,
+                'name' => $faker->name,
+                'username' => $faker->userName,
+                'email' => $faker->safeEmail,
+                'password' => (new DefaultPasswordHasher)->hash('abc123'),
+                'created' => $faker->date($format = 'Y-m-d H:i:s'),
+                'modified' => $faker->date($format = 'Y-m-d H:i:s')
+            ];
+        }
+
+        $table = $this->table('users');
+        $table->insert($data)->save();
+    }
+}
